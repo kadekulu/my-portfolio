40ae8aee42a6b6aeb17a582c9e18803b6d10144a
kadekulu <kainnkadekulu@gmail.com>
2026-05-02 03:25:18 +0900

    Rapid update: 03:25:18

--- data.js
+++ data.js
@@ -1,1 +1,149 @@
-const ARTWORKS_DATA = [];
+const ARTWORKS_DATA = [
+    {
+        "filename": "3f6c409b-67db-433d-bfd8-076a9d0720dd - コピー.jpg",
+        "title": "3f6c409b-67db-433d-bfd8-076a9d0720dd - コピー",
+        "date": "2026.04.30",
+        "tags": [],
+        "timestamp": 1777479092.0
+    },
+    {
+        "filename": "4f114598-7112-42f2-99d2-95bf2e4a02c8 - コピー.jpg",
+        "title": "4f114598-7112-42f2-99d2-95bf2e4a02c8 - コピー",
+        "date": "2026.04.30",
+        "tags": [],
+        "timestamp": 1777478438.0
+    },
+    {
+        "filename": "1db5acb2-efcf-4514-9d3e-852dd6854938 - コピー.jpg",
+        "title": "1db5acb2-efcf-4514-9d3e-852dd6854938 - コピー",
+        "date": "2026.04.29",
+        "tags": [],
+        "timestamp": 1777450248.2640965
+    },
+    {
+        "filename": "6a40a69f-b4a8-40f9-a74e-2f4ebd768beb - コピー.jpg",
+        "title": "6a40a69f-b4a8-40f9-a74e-2f4ebd768beb - コピー",
+        "date": "2026.04.27",
+        "tags": [],
+        "timestamp": 1777220776.929057
+    },
+    {
+        "filename": "5ea94838-53f0-4bb1-8780-76639d047480 - コピー.jpg",
+        "title": "5ea94838-53f0-4bb1-8780-76639d047480 - コピー",
+        "date": "2026.04.26",
+        "tags": [],
+        "timestamp": 1777143019.7145538
+    },
+    {
+        "filename": "8e93fe28-b664-4df0-b960-73b1349d868f - コピー.jpg",
+        "title": "8e93fe28-b664-4df0-b960-73b1349d868f - コピー",
+        "date": "2026.04.26",
+        "tags": [],
+        "timestamp": 1777142390.7098877
+    },
+    {
+        "filename": "2ba9c682-f010-4c10-9bb1-5dfcc50d3cd5 - コピー.jpg",
+        "title": "2ba9c682-f010-4c10-9bb1-5dfcc50d3cd5 - コピー",
+        "date": "2026.04.24",
+        "tags": [],
+        "timestamp": 1776958032.5807343
+    },
+    {
+        "filename": "8e6ecf22-f4be-45f8-9c4f-670029fc9338 - コピー.jpg",
+        "title": "8e6ecf22-f4be-45f8-9c4f-670029fc9338 - コピー",
+        "date": "2026.04.24",
+        "tags": [],
+        "timestamp": 1776958006.9433312
+    },
+    {
+        "filename": "4c09329e-0487-4a62-a8f1-2e5d3e035468.jpg",
+        "title": "4c09329e-0487-4a62-a8f1-2e5d3e035468",
+        "date": "2026.04.24",
+        "tags": [],
+        "timestamp": 1776956751.713122
+    },
+    {
+        "filename": "7cc1b987-27c6-4d9a-b917-225e6bc9bbc9 - コピー.jpg",
+        "title": "7cc1b987-27c6-4d9a-b917-225e6bc9bbc9 - コピー",
+        "date": "2026.04.23",
+        "tags": [],
+        "timestamp": 1776956197.9383118
+    },
+    {
+        "filename": "3c6d5f75-13d0-4f81-8355-71bbfaed8f72 - コピー.jpg",
+        "title": "3c6d5f75-13d0-4f81-8355-71bbfaed8f72 - コピー",
+        "date": "2026.04.21",
+        "tags": [],
+        "timestamp": 1776766265.9380908
+    },
+    {
+        "filename": "4fe3384c-c6a9-4f4b-be3a-301272adf79c.jpg",
+        "title": "4fe3384c-c6a9-4f4b-be3a-301272adf79c",
+        "date": "2026.04.19",
+        "tags": [],
+        "timestamp": 1776585136.8243902
+    },
+    {
+        "filename": "5c77367a-6024-41e2-a333-58d457834274.jpg",
+        "title": "5c77367a-6024-41e2-a333-58d457834274",
+        "date": "2026.04.18",
+        "tags": [],
+        "timestamp": 1776512455.182305
+    },
+    {
+        "filename": "3d0826a0-e311-447e-91bc-e79858f0c96f.jpg",
+        "title": "3d0826a0-e311-447e-91bc-e79858f0c96f",
+        "date": "2026.04.18",
+        "tags": [],
+        "timestamp": 1776512132.7499185
+    },
+    {
+        "filename": "3b7f5f7e-dbd5-4057-9855-b146c5481ac9.jpg",
+        "title": "3b7f5f7e-dbd5-4057-9855-b146c5481ac9",
+        "date": "2026.04.16",
+        "tags": [],
+        "timestamp": 1776340704.0023606
+    },
+    {
+        "filename": "2e131dc9-233b-4ae5-9c21-fb92362e73ed.jpg",
+        "title": "2e131dc9-233b-4ae5-9c21-fb92362e73ed",
+        "date": "2026.04.16",
+        "tags": [],
+        "timestamp": 1776339557.4990754
+    },
+    {
+        "filename": "6d1b88a3-6a00-48c3-919a-76fbca127745.jpg",
+        "title": "6d1b88a3-6a00-48c3-919a-76fbca127745",
+        "date": "2026.04.13",
+        "tags": [],
+        "timestamp": 1776015137.6706505
+    },
+    {
+        "filename": "2a7a9f7d-0a1e-4381-8b85-fc3ecf83b89d.jpg",
+        "title": "2a7a9f7d-0a1e-4381-8b85-fc3ecf83b89d",
+        "date": "2026.04.13",
+        "tags": [],
+        "timestamp": 1776012067.7269797
+    },
+    {
+        "filename": "1ed6d3ab-c446-45f2-b92a-aa1084dffb61.jpg",
+        "title": "1ed6d3ab-c446-45f2-b92a-aa1084dffb61",
+        "date": "2026.04.13",
+        "tags": [],
+        "timestamp": 1776010686.7445192
+    },
+    {
+        "filename": "2f62b0f7-119c-4e26-aa51-0eeeb7b52a16.jpg",
+        "title": "2f62b0f7-119c-4e26-aa51-0eeeb7b52a16",
+        "date": "2026.04.11",
+        "tags": [],
+        "timestamp": 1775896325.5973709
+    },
+    {
+        "filename": "8d2d44f4-2dfb-44bd-8e20-a4ac62949462_cleanup.jpg",
+        "title": "8d2d44f4-2dfb-44bd-8e20-a4ac62949462 cleanup",
+        "date": "2026.04.11",
+        "tags": [],
+        "timestamp": 1775841347.2398906
+    }
+];
