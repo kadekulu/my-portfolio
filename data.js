08f1df81f5c7e71df5881eb750b1b8c4c989beb2
kadekulu <kainnkadekulu@gmail.com>
2026-05-03 02:33:13 +0900

    Processed: 2ba9c682-f010-4c10-9bb1-5dfcc50d3cd5 - コピー.jpg: 02:33:13

--- data.js
+++ data.js
@@ -224,7 +224,12 @@ const ARTWORKS_DATA = [
         "filename": "2ba9c682-f010-4c10-9bb1-5dfcc50d3cd5 - コピー.jpg",
         "title": "2ba9c682-f010-4c10-9bb1-5dfcc50d3cd5 - コピー",
         "date": "2026.04.24",
-        "tags": [],
+        "tags": [
+            "Black Hair",
+            "Twin Tails",
+            "Lingerie",
+            "Original"
+        ],
         "timestamp": 1776958032.5807343
     },
     {
