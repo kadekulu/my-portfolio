7c1156fe81857116d4f9fd3532e534abcd815e7c
kadekulu <kainnkadekulu@gmail.com>
2026-05-03 22:43:10 +0900

    Rapid update: 22:43:10

--- data.js
+++ data.js
@@ -4,14 +4,14 @@ const ARTWORKS_DATA = [
         "title": "8e6ecf22-f4be-45f8-9c4f-670029fc9338 - コピー",
         "date": "2026.05.03",
         "tags": [],
-        "timestamp": 1777815778.6533906
+        "timestamp": 1777815786.161879
     },
     {
         "filename": "8d2d44f4-2dfb-44bd-8e20-a4ac62949462_cleanup.jpg",
         "title": "8d2d44f4-2dfb-44bd-8e20-a4ac62949462_cleanup",
         "date": "2026.05.03",
         "tags": [],
-        "timestamp": 1777815776.1185188
+        "timestamp": 1777815784.89003
     },
     {
         "filename": "894d9263-810e-42f0-9104-c30dd27453be.jpg",
