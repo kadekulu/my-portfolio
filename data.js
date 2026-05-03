1431a4ef08ecac6e264586b81316a2ef778c7f96
kadekulu <kainnkadekulu@gmail.com>
2026-05-03 16:01:05 +0900

    Tags cleaned: 450592b6-ecff-483e-b9cc-882274d46c84.jpg: 16:01:05

--- data.js
+++ data.js
@@ -149,7 +149,12 @@ const ARTWORKS_DATA = [
         "filename": "450592b6-ecff-483e-b9cc-882274d46c84.jpg",
         "title": "450592b6-ecff-483e-b9cc-882274d46c84",
         "date": "2026.05.03",
-        "tags": [],
+        "tags": [
+            "Pink Hair",
+            "Short Hair",
+            "Casual",
+            "Original"
+        ],
         "timestamp": 1777744557.457211
     },
     {
