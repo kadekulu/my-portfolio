d3a659f58384432013448921b1f7484c34910c64
kadekulu <kainnkadekulu@gmail.com>
2026-05-04 22:55:44 +0900

    Tags cleaned: aad2d867-3374-48df-949f-40f46ca04053 - コピー.jpg: 22:55:43

--- data.js
+++ data.js
@@ -3,7 +3,12 @@ const ARTWORKS_DATA = [
         "filename": "aad2d867-3374-48df-949f-40f46ca04053 - コピー.jpg",
         "title": "aad2d867-3374-48df-949f-40f46ca04053 - コピー",
         "date": "2026.05.04",
-        "tags": [],
+        "tags": [
+            "Pink Hair",
+            "Long Hair",
+            "Lingerie",
+            "Airi"
+        ],
         "timestamp": 1777902926.9900403
     },
     {
