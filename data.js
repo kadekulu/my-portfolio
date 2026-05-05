c3bee1fbc71cf196c838ed73b93495b4a9ffc579
kadekulu <kainnkadekulu@gmail.com>
2026-05-05 19:03:39 +0900

    Rapid update: 19:03:39

--- data.js
+++ data.js
@@ -4,7 +4,14 @@ const ARTWORKS_DATA = [
         "title": "9dd1d2c2-da4f-49e5-8aa6-bb8ec45ec952",
         "date": "2026.05.05",
         "tags": [],
-        "timestamp": 1777975400.15688
+        "timestamp": 1777975415.094344
+    },
+    {
+        "filename": "0875ebe2-b8f4-4648-a79c-611c3cc9e188.jpg",
+        "title": "0875ebe2-b8f4-4648-a79c-611c3cc9e188",
+        "date": "2026.05.05",
+        "tags": [],
+        "timestamp": 1777975407.0240192
     },
     {
         "filename": "d2715120-7605-44f7-b82d-478513abb015.jpg",
@@ -12,13 +19,6 @@ const ARTWORKS_DATA = [
         "date": "2026.05.05",
         "tags": [],
         "timestamp": 1777975397.0929213
-    },
-    {
-        "filename": "0875ebe2-b8f4-4648-a79c-611c3cc9e188.jpg",
-        "title": "0875ebe2-b8f4-4648-a79c-611c3cc9e188",
-        "date": "2026.05.05",
-        "tags": [],
-        "timestamp": 1777975393.8876512
     },
     {
         "filename": "a2651196-91bb-4594-8b4b-b5f1a49ba144.jpg",
