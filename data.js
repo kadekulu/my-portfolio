b049339daa5fc7f0e797213989d25b14f4b1c248
kadekulu <kainnkadekulu@gmail.com>
2026-05-06 14:02:36 +0900

    Tags cleaned: 22b09ce9-08d6-4b70-823d-16f8264b8d8e.jpg: 14:02:36

--- data.js
+++ data.js
@@ -10,7 +10,12 @@ const ARTWORKS_DATA = [
         "filename": "22b09ce9-08d6-4b70-823d-16f8264b8d8e.jpg",
         "title": "22b09ce9-08d6-4b70-823d-16f8264b8d8e",
         "date": "2026.05.06",
-        "tags": [],
+        "tags": [
+            "Pink Hair",
+            "Long Hair",
+            "Lingerie",
+            "Original"
+        ],
         "timestamp": 1778043740.0020216
     },
     {
