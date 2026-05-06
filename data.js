86bcfde1b12835e66a4d3da522aa87ff402cb3b7
kadekulu <kainnkadekulu@gmail.com>
2026-05-06 19:20:35 +0900

    Processed: 242192d5-fba9-4aa9-b653-07b278948b07.jpg: 19:20:35

--- data.js
+++ data.js
@@ -36,7 +36,12 @@ const ARTWORKS_DATA = [
         "filename": "242192d5-fba9-4aa9-b653-07b278948b07.jpg",
         "title": "242192d5-fba9-4aa9-b653-07b278948b07",
         "date": "2026.05.06",
-        "tags": [],
+        "tags": [
+            "Blue Hair",
+            "Long Hair",
+            "Lingerie",
+            "Airi"
+        ],
         "timestamp": 1778062797.4171054
     },
     {
