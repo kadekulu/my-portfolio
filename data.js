66daef888ed4eae68b81b346dafaaed7ec46ff31
kadekulu <kainnkadekulu@gmail.com>
2026-05-08 04:17:33 +0900

    Processed: 473a011e-3fb4-4988-a85e-a60c1d658ed0.jpg: 04:17:33

--- data.js
+++ data.js
@@ -51,7 +51,12 @@ const ARTWORKS_DATA = [
         "filename": "473a011e-3fb4-4988-a85e-a60c1d658ed0.jpg",
         "title": "473a011e-3fb4-4988-a85e-a60c1d658ed0",
         "date": "2026.05.06",
-        "tags": [],
+        "tags": [
+            "Pink Hair",
+            "Short Hair",
+            "Casual",
+            "Airi"
+        ],
         "timestamp": 1778077408.3068733
     },
     {
