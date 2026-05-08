07fe5a1f8eff534d7b7efb6986ac7688f60b488c
kadekulu <kainnkadekulu@gmail.com>
2026-05-09 03:35:25 +0900

    Processed tags: 2946e718-544d-4600-8bc9-a3210b6d4599.jpg: 03:35:25

--- data.js
+++ data.js
@@ -662,7 +662,12 @@ const ARTWORKS_DATA = [
         "filename": "2946e718-544d-4600-8bc9-a3210b6d4599.jpg",
         "title": "2946e718-544d-4600-8bc9-a3210b6d4599",
         "date": "2026.05.06",
-        "tags": [],
+        "tags": [
+            "Brown Hair",
+            "Long Hair",
+            "Lingerie",
+            "Original"
+        ],
         "timestamp": 1778062798.9879625
     },
     {
