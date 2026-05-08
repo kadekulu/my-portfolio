5c6c7fdce173efbca7ff1417d062e1c5b87381d0
kadekulu <kainnkadekulu@gmail.com>
2026-05-09 03:32:25 +0900

    Processed tags: 0875ebe2-b8f4-4648-a79c-611c3cc9e188.jpg: 03:32:25

--- data.js
+++ data.js
@@ -1,12 +1,5 @@
 const ARTWORKS_DATA = [
     {
-        "filename": "15cd9fd2-8e2f-496e-8f8c-5ce2d3c4740c.jpg",
-        "title": "15cd9fd2-8e2f-496e-8f8c-5ce2d3c4740c",
-        "date": "2026.05.09",
-        "tags": [],
-        "timestamp": 1778265136.1193774
-    },
-    {
         "filename": "143a306c-f387-475a-a061-bb0215c877ef.jpg",
         "title": "143a306c-f387-475a-a061-bb0215c877ef",
         "date": "2026.05.09",
@@ -31,7 +24,12 @@ const ARTWORKS_DATA = [
         "filename": "0875ebe2-b8f4-4648-a79c-611c3cc9e188.jpg",
         "title": "0875ebe2-b8f4-4648-a79c-611c3cc9e188",
         "date": "2026.05.09",
-        "tags": [],
+        "tags": [
+            "Pink Hair",
+            "Wavy Hair",
+            "Lingerie",
+            "Airi"
+        ],
         "timestamp": 1778265122.6506405
     },
     {
@@ -604,6 +602,13 @@ const ARTWORKS_DATA = [
         "date": "2026.05.06",
         "tags": [],
         "timestamp": 1778062710.5396748
+    },
+    {
+        "filename": "15cd9fd2-8e2f-496e-8f8c-5ce2d3c4740c.jpg",
+        "title": "15cd9fd2-8e2f-496e-8f8c-5ce2d3c4740c",
+        "date": "2026.05.06",
+        "tags": [],
+        "timestamp": 1778062701.9211655
     },
     {
         "filename": "da1d9336-e209-4ee9-aa5b-16c0212b6a42.jpg",
