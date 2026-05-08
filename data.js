0e1348bf14ad14398708da89bb86b58a9bac5f01
kadekulu <kainnkadekulu@gmail.com>
2026-05-09 00:48:24 +0900

    Update gallery data: 00:48:24

--- data.js
+++ data.js
@@ -4,35 +4,35 @@ const ARTWORKS_DATA = [
         "title": "083f748e-5b0e-49cd-a740-024556c15c1a",
         "date": "2026.05.09",
         "tags": [],
-        "timestamp": 1778255288.761733
+        "timestamp": 1778255300.9787927
     },
     {
         "filename": "06a6347e-d159-432c-b129-b0c192f1546d.jpg",
         "title": "06a6347e-d159-432c-b129-b0c192f1546d",
         "date": "2026.05.09",
         "tags": [],
-        "timestamp": 1778255287.19738
+        "timestamp": 1778255299.8898997
     },
     {
         "filename": "00115-611997926_cleanu.jpg",
         "title": "00115-611997926_cleanu",
         "date": "2026.05.09",
         "tags": [],
-        "timestamp": 1778255285.9782906
+        "timestamp": 1778255298.5425315
     },
     {
         "filename": "00087-4028545399_cleanup.jpg",
         "title": "00087-4028545399_cleanup",
         "date": "2026.05.09",
         "tags": [],
-        "timestamp": 1778255284.4899976
+        "timestamp": 1778255296.7287984
     },
     {
         "filename": "00029-4202640360_cleanup.jpg",
         "title": "00029-4202640360_cleanup",
         "date": "2026.05.09",
         "tags": [],
-        "timestamp": 1778255278.3102434
+        "timestamp": 1778255295.3016038
     },
     {
         "filename": "524f53df-b16e-481b-8f5f-399624f58a6b.jpg",
