df92724c7954fdf8b3beb247438cf129a529983b
kadekulu <kainnkadekulu@gmail.com>
2026-05-09 03:33:24 +0900

    Update gallery data: 03:33:24

--- data.js
+++ data.js
@@ -1,5 +1,26 @@
 const ARTWORKS_DATA = [
     {
+        "filename": "1ed6d3ab-c446-45f2-b92a-aa1084dffb61.jpg",
+        "title": "1ed6d3ab-c446-45f2-b92a-aa1084dffb61",
+        "date": "2026.05.09",
+        "tags": [],
+        "timestamp": 1778265198.3459592
+    },
+    {
+        "filename": "1db5acb2-efcf-4514-9d3e-852dd6854938 - コピー.jpg",
+        "title": "1db5acb2-efcf-4514-9d3e-852dd6854938 - コピー",
+        "date": "2026.05.09",
+        "tags": [],
+        "timestamp": 1778265195.1502805
+    },
+    {
+        "filename": "1c7f680a-91a4-420d-9e5d-a0983e933da5.jpg",
+        "title": "1c7f680a-91a4-420d-9e5d-a0983e933da5",
+        "date": "2026.05.09",
+        "tags": [],
+        "timestamp": 1778265192.2059915
+    },
+    {
         "filename": "180a2415-7b22-4496-950d-6a8282ed40c8.jpg",
         "title": "180a2415-7b22-4496-950d-6a8282ed40c8",
         "date": "2026.05.09",
@@ -384,13 +405,6 @@ const ARTWORKS_DATA = [
         "timestamp": 1778228940.2672937
     },
     {
-        "filename": "1c7f680a-91a4-420d-9e5d-a0983e933da5.jpg",
-        "title": "1c7f680a-91a4-420d-9e5d-a0983e933da5",
-        "date": "2026.05.08",
-        "tags": [],
-        "timestamp": 1778228929.909143
-    },
-    {
         "filename": "80b3301a-cfe0-4d1e-98c4-34b7120fcbd9.jpg",
         "title": "80b3301a-cfe0-4d1e-98c4-34b7120fcbd9",
         "date": "2026.05.08",
@@ -620,20 +634,6 @@ const ARTWORKS_DATA = [
         "date": "2026.05.06",
         "tags": [],
         "timestamp": 1778062774.1482222
-    },
-    {
-        "filename": "1ed6d3ab-c446-45f2-b92a-aa1084dffb61.jpg",
-        "title": "1ed6d3ab-c446-45f2-b92a-aa1084dffb61",
-        "date": "2026.05.06",
-        "tags": [],
-        "timestamp": 1778062759.7070162
-    },
-    {
-        "filename": "1db5acb2-efcf-4514-9d3e-852dd6854938 - コピー.jpg",
-        "title": "1db5acb2-efcf-4514-9d3e-852dd6854938 - コピー",
-        "date": "2026.05.06",
-        "tags": [],
-        "timestamp": 1778062745.0551205
     },
     {
         "filename": "da1d9336-e209-4ee9-aa5b-16c0212b6a42.jpg",
