7db105195e17f2d0d483d0e3ab0f44a19717af46
kadekulu <kainnkadekulu@gmail.com>
2026-05-18 03:05:15 +0900

    Update gallery data and captions: 03:05:15

--- data.js
+++ data.js
@@ -1,5 +1,18 @@
 const ARTWORKS_DATA = [
     {
+        "filename": "Noon/0000-2744945904.jpg",
+        "title": "0000-2744945904",
+        "date": "2026.05.18",
+        "tags": [
+            "Other",
+            "Other",
+            "Other",
+            "Original"
+        ],
+        "timestamp": 1779041056.7642949,
+        "time_zone": "昼"
+    },
+    {
         "filename": "07fb29bf-0bc4-474b-8e03-9e5fe18e526c.jpg",
         "title": "07fb29bf-0bc4-474b-8e03-9e5fe18e526c",
         "date": "2026.05.18",
@@ -507,8 +520,8 @@ const ARTWORKS_DATA = [
         "time_zone": "未指定"
     },
     {
-        "filename": "Noon/0000-2744945904.jpg",
-        "title": "0000-2744945904",
+        "filename": "Noon/00004-2744945904.jpg",
+        "title": "00004-2744945904",
         "date": "2026.05.18",
         "tags": [
             "Other",
@@ -516,7 +529,7 @@ const ARTWORKS_DATA = [
             "Other",
             "Original"
         ],
-        "timestamp": 1779031770.1042564,
+        "timestamp": 1779032135.84569,
         "time_zone": "昼",
         "captions": [
             "イラストを追加しました！\n\n\n#愛依莉",
