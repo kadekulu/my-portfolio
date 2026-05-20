94f3c8c0c08c3b7a2eb8aa57e5c223400c4c17ae
kadekulu <kainnkadekulu@gmail.com>
2026-05-21 06:37:12 +0900

    Update gallery data and captions: 06:37:12

--- data.js
+++ data.js
@@ -1,5 +1,23 @@
 const ARTWORKS_DATA = [
     {
+        "filename": "Night/829c8622-6d2a-48a6-a8e0-89717de09e23.jpg",
+        "title": "829c8622-6d2a-48a6-a8e0-89717de09e23",
+        "date": "2026.05.21",
+        "tags": [
+            "Other",
+            "Other",
+            "Other",
+            "Original"
+        ],
+        "timestamp": 1779313017.7606876,
+        "time_zone": "夜",
+        "captions": [
+            "イラストを追加しました！\n\n\n#愛依莉",
+            "イラストを追加しました！\n\n\n#愛依莉",
+            "イラストを追加しました！\n\n\n#愛依莉"
+        ]
+    },
+    {
         "filename": "Night/7b93f73c-da0f-4e74-aefa-a599d3e01f39.jpg",
         "title": "7b93f73c-da0f-4e74-aefa-a599d3e01f39",
         "date": "2026.05.20",
@@ -9,13 +27,8 @@ const ARTWORKS_DATA = [
             "Other",
             "Original"
         ],
-        "timestamp": 1779212063.4167202,
-        "time_zone": "夜",
-        "captions": [
-            "イラストを追加しました！\n\n\n#愛依莉",
-            "イラストを追加しました！\n\n\n#愛依莉",
-            "イラストを追加しました！\n\n\n#愛依莉"
-        ]
+        "timestamp": 1779212113.851684,
+        "time_zone": "夜"
     },
     {
         "filename": "Midnight/a46161f4-cabd-489d-874e-657b852fcc1c.jpg",
