fa2ee8be3ad2459b35f58a7c290621388c4984e8
kadekulu <kainnkadekulu@gmail.com>
2026-06-05 03:07:32 +0900

    Update gallery data and captions: 03:07:32

--- data.js
+++ data.js
@@ -1,5 +1,23 @@
 const ARTWORKS_DATA = [
     {
+        "filename": "Noon/a9a7bc95-5935-4f4f-ac73-aae2996f23ca.jpg",
+        "title": "a9a7bc95-5935-4f4f-ac73-aae2996f23ca",
+        "date": "2026.06.05",
+        "tags": [
+            "Blonde Hair",
+            "Long Hair",
+            "Lingerie",
+            "Original"
+        ],
+        "timestamp": 1780596421.1743548,
+        "time_zone": "昼",
+        "captions": [
+            "美しい髪\n\n\n#愛依莉",
+            "清楚な雰囲気\n\n\n#愛依莉",
+            "華やかな衣装\n\n\n#愛依莉"
+        ]
+    },
+    {
         "filename": "Morning/08356e38-e276-4710-8d7e-99ab3e95b6d1.jpg",
         "title": "08356e38-e276-4710-8d7e-99ab3e95b6d1",
         "date": "2026.06.05",
@@ -9,13 +27,8 @@ const ARTWORKS_DATA = [
             "Dress",
             "Airi"
         ],
-        "timestamp": 1780593602.0448623,
-        "time_zone": "朝",
-        "captions": [
-            "優しい笑顔\n\n\n#愛依莉",
-            "天使の翼\n\n\n#愛依莉",
-            "青いドレス\n\n\n#愛依莉"
-        ]
+        "timestamp": 1780593603.8508613,
+        "time_zone": "朝"
     },
     {
         "filename": "Morning/976d1a81-b885-49d5-986d-711f495ac37e.jpg",
