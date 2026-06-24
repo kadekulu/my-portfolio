9bd70df36c847002912442818dc2b45abb74ab87
kadekulu <kainnkadekulu@gmail.com>
2026-06-24 18:55:43 +0900

    Update gallery data and captions: 18:55:43

--- data.js
+++ data.js
@@ -1,10 +1,18 @@
 const ARTWORKS_DATA = [
     {
+        "filename": "Night/00052-1450663787.jpg",
+        "title": "00052-1450663787",
+        "date": "2026.06.24",
+        "tags": [],
+        "timestamp": 1782294871.8205173,
+        "time_zone": "夜"
+    },
+    {
         "filename": "Night/00048-3515688541.jpg",
         "title": "00048-3515688541",
         "date": "2026.06.24",
         "tags": [],
-        "timestamp": 1782294525.0113883,
+        "timestamp": 1782294556.8870392,
         "time_zone": "夜"
     },
     {
