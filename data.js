e3aaf4cf1c2a1cc99881eaee3212e5f782119d15
kadekulu <kainnkadekulu@gmail.com>
2026-07-23 19:04:04 +0900

    Update gallery data and captions: 19:04:04

--- data.js
+++ data.js
@@ -1,10 +1,18 @@
 const ARTWORKS_DATA = [
     {
+        "filename": "Morning/00011-3935294467.jpg",
+        "title": "00011-3935294467",
+        "date": "2026.07.23",
+        "tags": [],
+        "timestamp": 1784800951.8533247,
+        "time_zone": "朝"
+    },
+    {
         "filename": "Morning/00000-531331354_cleanup.jpg",
         "title": "00000-531331354_cleanup",
         "date": "2026.07.23",
         "tags": [],
-        "timestamp": 1784798688.435457,
+        "timestamp": 1784798689.4325688,
         "time_zone": "朝"
     },
     {
